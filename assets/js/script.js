0f10a70aac2794176472e2cbe0a1b5d893767497
npokrandt <npokrandt919@gmail.com>
2023-07-07 22:46:22 -0500

    data entered into a timebox can now be saved by clicking the 'save' button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,10 @@
 // THEN I am presented with timeblocks for standard business hours of 9am-5pm - check
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future - check
-
-
 // WHEN I click into a timeblock
-// THEN I can enter an event - ??
+// THEN I can enter an event - check
+
+
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page 
@@ -41,38 +41,26 @@ $(function () {
   //updating the date header
   var timeHeader = $('#currentDay')
 
-  timeHeader.text(today)
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  timeHeader.text(today) 
   //the classes are 'past', 'present' and 'future'
   colorTimeBoxes()
 
   function colorTimeBoxes(){
-
+    
     //this will provide the current hour
     var currentHour = dayjs().hour()
-    console.log(typeof(currentHour))
+    //console.log(typeof(currentHour))
 
     for (var i = 9; i < 18; i++){
-      var hourId = JSON.stringify(i)
-      var timeBox = $("#hour-" + hourId)
+      var hourId = i
+      //for getting each element by id
+      var hourIdStr = JSON.stringify(i)
+      var timeBox = $("#hour-" + hourIdStr)
 
-      if (JSON.parse(hourId) < currentHour){
+      if (hourId < currentHour){
         //console.log(timeBox.attr("id"))
         timeBox.addClass("past")
-      } else if (JSON.parse(hourId) === currentHour){
+      } else if (hourId === currentHour){
         timeBox.addClass("present")
       } else {
         timeBox.addClass("future")
@@ -80,7 +68,17 @@ $(function () {
       //console.log(timeboxtest.attr('id'))
     }
   }
-   
+  
+    function saveData(){
+      var parentDiv = $(this).parents("div.row")
+      var id = parentDiv.attr("id")
+      var eventToBeSaved = parentDiv.children("textarea").val()
+      if (eventToBeSaved != ""){
+        localStorage.setItem(id, eventToBeSaved)
+      }
+    }
+
+  $(".btn").click(saveData)
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
